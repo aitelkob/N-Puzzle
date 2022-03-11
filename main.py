44da9f7cd60cbb5cb4dc65f29dc0cfb4e78be416
Othmane Aghzaf <oaghzaf@e2r13p3.1337.ma>
2022-03-11 16:22:12 +0100

    map Check

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
+from re import S
+import re
 import sys
 
 
 arv = sys.argv[1]
-len = 0
+length = 0
 map = []
+
+def clearComments(data):
+    commentStart = 0
+    cleanData = []
+    for item in data:
+        if item.find("#") != -1:
+            commentStart = item.find("#")
+            string = item[:commentStart]
+            if string != '':
+                cleanData.append(string)
+        else:
+            cleanData.append(item)
+    return cleanData
+
+def mapChecker(data, length):
+    map = data[1:]
+    regexString = '\d+\s' * length
+    regexString = regexString[:-2]
+    print("-->", regexString)
+    for item in map:
+        if re.search(regexString, item) is None:
+            print("item :", item)
+            return False
+    return True
 
 with open(arv) as file:
     string = file.read()
     data = string.split("\n")
-    len = data[1]
-    
-    # cleanData = []
-    # for item in data:
-    #     if item.startswith("#"):
-    #         continue
-    #     else
+    cleanData = clearComments(data)
+    length = int(cleanData[0])
+    if mapChecker(cleanData, length) is False:
+        print("the map you introduced is invalid")
+
 
 print(data)
+print(cleanData)
