4d6546d7eba9eca605461e0581ba33b657fdcbcd
Othmane Aghzaf <oaghzaf@e1r7p7.1337.ma>
2022-03-13 11:56:46 +0100

    Goal generator

---
+++ generator.py
@@ -0,0 +1,45 @@
+def createPuzzle(size):
+    puzzle = []
+    for number in range(0, size):
+        puzzle.append([0] * size)
+    
+    i, j = 0, 0
+    number = 0
+    xsize = size - 1
+    ysize = size - 1
+    magic = 0
+    while magic < size:
+        i = magic
+        j = magic
+        while j <= ysize - magic:
+            puzzle[i][j] = number + 1
+            number += 1
+            j += 1
+        i += 1
+        j -= 1
+        while i <= xsize - magic:
+            puzzle[i][j] = number + 1
+            number += 1
+            i += 1
+        j -= 1
+        i -= 1
+        while j >= magic:
+            puzzle[i][j] = number + 1
+            number += 1
+            j -= 1
+        j += 1
+        i -= 1
+        while i > magic:
+            puzzle[i][j] = number + 1
+            number += 1
+            i -= 1
+        magic += 1
+    
+    i, j = 0, 0
+    while i < size:
+        j = 0
+        while j < size:
+            if puzzle[i][j] == (size * size):
+                puzzle[i][j] = 0
+            j += 1
+        i += 1
